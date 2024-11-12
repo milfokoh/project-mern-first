e74aea02e10a14ef28f9d5dd7d34067378f13217
Troshina Alesya <a.trowina@gmail.com>
2024-11-12 16:49:16 +0500

    test commit

--- client/src/hooks/auth.hook.js
+++ client/src/hooks/auth.hook.js
@@ -1,38 +1,41 @@
-import { useState, useCallback, useEffect } from "react"
+import { useState, useCallback, useEffect } from 'react';
 
 const storageName = 'userData';
 
 export const useAuth = () => {
-    const [token, setToken] = useState(null);
-    const [ready, setReady] = useState(false);
-    const [userId, setUserId] = useState(null);
+	const [token, setToken] = useState(null);
+	const [ready, setReady] = useState(false);
+	const [userId, setUserId] = useState(null);
 
-    const login = useCallback( (jwtToken, idd) => {
-        setToken(jwtToken);
-        setUserId(idd);
+	const login = useCallback((jwtToken, idd) => {
+		setToken(jwtToken);
+		setUserId(idd);
 
-        localStorage.setItem(storageName, JSON.stringify({
-            userId: idd, token: jwtToken
-        }))
-    },[]);
+		localStorage.setItem(
+			storageName,
+			JSON.stringify({
+				userId: idd,
+				token: jwtToken,
+			})
+		);
+	}, []);
 
-    const logout = useCallback( () => {
-        setToken(null);
-        setUserId(null);
-        localStorage.removeItem(storageName);
-    }, []);
+	const logout = useCallback(() => {
+		setToken(null);
+		setUserId(null);
+		localStorage.removeItem(storageName);
+	}, []);
 
-    useEffect( () => {
-        const data = JSON.parse(localStorage.getItem(storageName));
+	useEffect(() => {
+		const data = JSON.parse(localStorage.getItem(storageName));
+		console.log('[Im test]');
 
-        if (data && data.token) {
-            login(data.token, data.userId)
-        }
+		if (data && data.token) {
+			login(data.token, data.userId);
+		}
 
-        setReady(true);
-    }, [login]);
+		setReady(true);
+	}, [login]);
 
-    return { login, logout, token, userId, ready};
-    
-
-}
+	return { login, logout, token, userId, ready };
+};

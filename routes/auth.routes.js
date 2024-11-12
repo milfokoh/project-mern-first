89164d8bdcdc483d0e8f5de2249e4fb27f3658da
Troshina Alesya <a.trowina@gmail.com>
2024-11-12 16:59:59 +0500

    test commit#3

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,95 +1,101 @@
-const {Router} = require('express')
-const bcrypt = require('bcryptjs')
-const config = require('config')
-const jwt = require('jsonwebtoken')
-const {check, validationResult} = require('express-validator')
-const User = require('../models/User')
-const router = Router()
-
+const { Router } = require('express');
+const bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
+const User = require('../models/User');
+const router = Router();
 
 // /api/auth/register
 router.post(
-    '/register',
-    [
-        check('email', 'Некорректный email').isEmail(),
-        check('password', 'Минимальная длина пароля 6 символов')
-            .isLength({ min: 6 })
-    ],
-    async (req, res) => {
-        try {
-            const errors = validationResult(req)
+	'/register',
+	[
+		check('email', 'Некорректный email').isEmail(),
+		check('password', 'Минимальная длина пароля 6 символов').isLength({
+			min: 6,
+		}),
+	],
+	async (req, res) => {
+		try {
+			const errors = validationResult(req);
 
-            if (!errors.isEmpty()) {
-                return res.status(400).json({
-                    errors: errors.array(),
-                    message: 'Некорректный данные при регистрации'
-                })
-            }
+			if (!errors.isEmpty()) {
+				return res.status(400).json({
+					errors: errors.array(),
+					message: 'Некорректный данные при регистрации',
+				});
+			}
 
-            const {email, password} = req.body
+			const { email, password } = req.body;
 
-            const candidate = await User.findOne({ email })
+			const candidate = await User.findOne({ email });
 
-            if (candidate) {
-                return res.status(400).json({ message: 'Такой пользователь уже существует' })
-            }
+			if (candidate) {
+				return res
+					.status(400)
+					.json({ message: 'Такой пользователь уже существует' });
+			}
 
-            const hashedPassword = await bcrypt.hash(password, 12)
-            const user = new User({ email, password: hashedPassword })
+			const hashedPassword = await bcrypt.hash(password, 12);
+			const user = new User({ email, password: hashedPassword });
 
-            await user.save()
+			await user.save();
 
-            res.status(201).json({ message: 'Пользователь создан' })
-
-        } catch (e) {
-            res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова(auth.routes)' })
-        }
-    })
+			res.status(201).json({ message: 'Пользователь создан' });
+		} catch (e) {
+			res.status(500).json({
+				message: 'Что-то пошло не так, попробуйте снова(auth.routes)',
+			});
+		}
+	}
+);
 
 // /api/auth/login
 router.post(
-    '/login',
-    [
-        check('email', 'Введите корректный email').normalizeEmail().isEmail(),
-        check('password', 'Введите пароль').exists()
-    ],
-    async (req, res) => {
-        try {
-            const errors = validationResult(req)
+	'/login',
+	[
+		check('email', 'Введите корректный email').normalizeEmail().isEmail(),
+		check('password', 'Введите пароль').exists(),
+	],
+	async (req, res) => {
+		try {
+			const errors = validationResult(req);
 
-            if (!errors.isEmpty()) {
-                return res.status(400).json({
-                    errors: errors.array(),
-                    message: 'Некорректный данные при входе в систему'
-                })
-            }
+			if (!errors.isEmpty()) {
+				return res.status(400).json({
+					errors: errors.array(),
+					message: 'Некорректный данные при входе в систему',
+				});
+			}
 
-            const {email, password} = req.body
+			const { email, password } = req.body;
 
-            const user = await User.findOne({ email })
+			const user = await User.findOne({ email });
 
-            if (!user) {
-                return res.status(400).json({ message: 'Пользователь не найден' })
-            }
+			if (!user) {
+				return res.status(400).json({ message: 'Пользователь не найден' });
+			}
 
-            const isMatch = await bcrypt.compare(password, user.password)
+			const isMatch = await bcrypt.compare(password, user.password);
 
-            if (!isMatch) {
-                return res.status(400).json({ message: 'Неверный пароль, попробуйте снова' })
-            }
+			if (!isMatch) {
+				return res
+					.status(400)
+					.json({ message: 'Неверный пароль, попробуйте снова' });
+			}
 
-            const token = jwt.sign(
-                { userId: user.id },
-                config.get('jwtSecret'),
-                { expiresIn: '1h' }
-            )
+			const token = jwt.sign({ userId: user.id }, config.get('jwtSecret'), {
+				expiresIn: '1h',
+			});
 
-            res.json({ token, userId: user.id })
+			res.json({ token, userId: user.id });
+		} catch (e) {
+			res.status(500).json({
+				message: 'Что-то пошло не так, попробуйте снова(auth.routes)',
+			});
+		}
+	}
+);
 
-        } catch (e) {
-            res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова(auth.routes)' })
-        }
-    })
-
-
-module.exports = router
+module.exports = router;
+console.log('[Im test]');

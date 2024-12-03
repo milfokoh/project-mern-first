becfaba571e2d977865be614c104ba7415af7d92
Troshina Alesya <a.trowina@gmail.com>
2024-12-03 18:35:52 +0500

    Пробую закомитить с несуществующей ветки в удаленном репозитории, на существующую но не имеющую никакого отношения (общего названия) к локальной

--- client/src/components/LinksList.js
+++ client/src/components/LinksList.js
@@ -1,18 +1,13 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-
-export const LinksList = (links) => {
-    if (links.length) {
-        return <p className="center">
-            Информации пока нет...
-        </p>
-
-    }
-    return(
-        <div className="row-nav-page" >
-            <p className="p-page-info">Пока данных нет, но скоро появяться</p>
-        </div>
-
-)
-}
+export const LinksList = links => {
+	if (links.length) {
+		return <p className='center'>Информации пока нет...</p>;
+	}
+	return (
+		<div className='row-nav-page'>
+			<p className='p-page-info'>Пока данных нет и не появятся!</p>
+		</div>
+	);
+};
